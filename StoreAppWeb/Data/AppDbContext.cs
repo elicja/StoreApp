b1a131e34c984a53e1515708324297b88532d540
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-06 22:32:23 +0200

    EF configuration

---
+++ StoreAppWeb/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreAppWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
+        {
+            
+        }
+    }
+}
--- StoreAppWeb/Program.cs
+++ StoreAppWeb/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using StoreAppWeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opts => 
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 
