ecdcd6a5d28981390688a2ae5c585ad64ba6af18
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-07 14:54:29 +0200

    categories display

---
+++ StoreAppWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreAppWeb.Data;
+using StoreAppWeb.Models;
+
+namespace StoreAppWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
