f6cce5413dd3900e2d86f1a54e3d6fe4e9deaea4
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-15 14:14:41 +0200

    Add product examples to db set

--- DataAccess/Migrations/20230915121415_AddProductsToDbAndSeed.Designer.cs
+++ DataAccess/Migrations/20230915121415_AddProductsToDbAndSeed.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoreApp.DataAccess.Data;
 
@@ -10,9 +11,11 @@ using StoreApp.DataAccess.Data;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230915121415_AddProductsToDbAndSeed")]
+    partial class AddProductsToDbAndSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
