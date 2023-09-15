d78ce4156154131dbb4cb9cc0dcd46e2d07d4ac7
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-15 11:13:52 +0200

    generic crud implementation

---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApp.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteFew(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+
+            return query.ToList();
+        }
+    }
+}
