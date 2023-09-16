50132bfae1a99af543636f778b84fc6d1d7d3fa7
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-16 20:12:06 +0200

    foreign key for product-category relation

--- DataAccess/Data/AppDbContext.cs
+++ DataAccess/Data/AppDbContext.cs
@@ -33,7 +33,8 @@ namespace StoreApp.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId = 1
                 },
 
                 new Product()
@@ -46,7 +47,8 @@ namespace StoreApp.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2
                 },
 
                 new Product()
@@ -59,7 +61,8 @@ namespace StoreApp.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
+                    Price100 = 35,
+                    CategoryId = 3
                 },
 
                 new Product()
@@ -72,7 +75,8 @@ namespace StoreApp.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
+                    Price100 = 55,
+                    CategoryId = 3
                 },
 
                 new Product()
@@ -85,7 +89,8 @@ namespace StoreApp.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2
                 },
 
                 new Product()
@@ -98,7 +103,8 @@ namespace StoreApp.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2
                 });
         }
     }
--- Models/Product.cs
+++ Models/Product.cs
@@ -5,6 +5,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StoreApp.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
@@ -43,5 +45,10 @@ namespace Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
