85ba8e70f0cb0c9a68a1f22fca8b2c832872749c
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-17 21:40:26 +0200

    improving updating method

--- StoreAppWeb/Areas/Admin/Controllers/ProductController.cs
+++ StoreAppWeb/Areas/Admin/Controllers/ProductController.cs
@@ -63,6 +63,17 @@ namespace StoreAppWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"imgs\product");
 
+                    if (!string.IsNullOrEmpty(productVM.Product.ImgUrl))
+                    {
+                        var oldImgPath = 
+                            Path.Combine(wwwRootPath, productVM.Product.ImgUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImgPath))
+                        {
+                            System.IO.File.Delete(oldImgPath);
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
@@ -71,7 +82,15 @@ namespace StoreAppWeb.Areas.Admin.Controllers
                     productVM.Product.ImgUrl = @"\imgs\product\" + fileName;
                 }
 
-                _unitOfWork.ProductRepo.Add(productVM.Product);
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.ProductRepo.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.ProductRepo.Update(productVM.Product);
+                }
+                
                 _unitOfWork.Save();
 
                 TempData["success"] = "Product created successfully";
