75ad2ace4da6629124849d4aa27e43795fb53199
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-19 20:37:50 +0200

    OrderDetails properties

--- Models/OrderDetails.cs
+++ Models/OrderDetails.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,23 @@ namespace Models
 {
     public class OrderDetails
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderHeaderId { get; set; }
+
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public double Price { get; set; }
     }
 }
