e330a6d7bb12a11659aa0ef38aff00203bd905ff
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-19 20:34:09 +0200

    Order Header properties

---
+++ Models/OrderHeader.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        public string AppUserId { get; set; }
+
+        [ForeignKey("AppUserId")]
+        [ValidateNever]
+        public AppUser AppUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateOnly PaymentDueDate { get; set; }
+        public double OrderTotal { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+        public string? PaymentId { get; set; }
+
+        [Required]
+        public string PhonNumber { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
