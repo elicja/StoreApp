6c9f58fe3eb2d80bdb5105eb116a351a9c33a6bc
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-19 19:43:42 +0200

    shopping cart view model

--- StoreAppWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ StoreAppWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.IRepositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Models.ViewModels;
+using System.Security.Claims;
 
 namespace StoreAppWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class ShoppingCartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCartRepo.GetAll(s => s.AppUserId == userId,
+                    includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
