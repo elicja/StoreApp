fc87863a232da3756fd7e3407858d6ffe09f8132
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-19 12:46:20 +0200

    fixes

--- StoreAppWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ StoreAppWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,6 +109,15 @@ namespace StoreAppWeb.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            public string? StreetAddres { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -145,6 +154,14 @@ namespace StoreAppWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                user.Name = Input.Name;
+                user.StreetAddres = Input.StreetAddres;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
