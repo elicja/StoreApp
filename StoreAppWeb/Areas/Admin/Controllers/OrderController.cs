2d69d9507c99373cbdb1e788c776e10e8cc1a255
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-20 19:33:38 +0200

    Order Controller and view model

---
+++ StoreAppWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.IRepositories;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace StoreAppWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region ApiCalls
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> orderHeaders = _unitOfWork.OrderHeaderRepo.GetAll(includeProperties: "AppUser").ToList();
+
+			return Json(new { data = orderHeaders });
+		}
+
+		#endregion
+	}
+}
