4ee07e41fb94e0cb1931b666edf127d3bdf6e086
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-09-22 13:12:47 +0200

    user controller

---
+++ StoreAppWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.IRepositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.ViewModels;
+using NuGet.Common;
+using StoreApp.DataAccess.Data;
+using StoreApp.Models;
+using Utility;
+
+namespace StoreAppWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public UserController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region ApiCalls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<AppUser> usersList = _db.AppUsers.Include(u => u.company).ToList();
+
+            return Json(new { data = usersList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
